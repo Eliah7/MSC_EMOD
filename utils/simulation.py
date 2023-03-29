f47b37f0156f7c37c82706b6385986ae32305af1
sylivera.massawe@vodacom.co.tz <sylivera.massawe@vodacom.co.tz>
2023-03-29 16:53:47 +0300

    access to cmp files

--- utils/simulation.py
+++ utils/simulation.py
@@ -5,7 +5,7 @@ def run_simulation():
     - compute the reward and return the reward
     """
     print("\nStarting Simulation...\n")
-    execute_docker_commands()
+    return execute_docker_commands()
     # compute reward
     # consider states as input to the simulation
     # find out how states will be represented
@@ -26,5 +26,5 @@ def execute_docker_commands(*commands):
     # print(f"{os.getcwd()}")
     cont = client.containers.run(image="idm:v3", volumes=[f"{os.getcwd()}/data:/EMOD"], command=["-c", "/Eradication -C config.json"], detach=True)
     cont.wait()
-    # print(cont.logs().decode('UTF-8'))
+    return cont.logs().decode('UTF-8')
     # print(client.containers.list())
